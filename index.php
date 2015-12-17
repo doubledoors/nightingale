ddd1ab37f693f94be4aacc77d4b40fd51397d644
Ben Sharp <bfwsharp@gmail.com>
2015-12-17 22:20:12 +0000

    build
    add fonts

--- index.php
+++ index.php
@@ -64,7 +64,11 @@ require_once('phpmailer/mail.php');
     <!--[if lt IE 8]>
         <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
+    <nav>
+        
+    </nav>
     <header>
+      <!-- <div id="top-bar"></div>   -->
       <div id="header-content">
         <div id="header-content--middle">
           <div id="header-content--inner">
